df8a8a4d6ab83a8ca2dacfb06975db6481f1e03c
Redha Alaraibi <reda9808@gmail.com>
2024-08-08 11:04:22 +0300

    Finla Result

---
+++ src/components/example-3-css-in-js/Button.jsx
@@ -0,0 +1,29 @@
+// src/components/example-3-css-in-js/Button.jsx
+
+import { createUseStyles } from "react-jss";
+
+
+    // src/components/example-3-css-in-js/Button.jsx
+
+const Button = ({ buttonText }) => {
+
+  const styles = createUseStyles({
+    btn: {
+      backgroundColor: '#24292e', // adding the styles in the object that we'll apply to our button
+      borderRadius: '8px',
+      color: '#f7df1c',
+      fontSize: '24pt',
+      padding: '15px 50px',
+      width: '300px',
+    },
+  });
+
+  const classes = styles();
+  return (
+    <button className={classes.btn}>
+      {buttonText}
+    </button>
+  );
+};
+
+export default Button;
